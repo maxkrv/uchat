e010250c07b5caa0343450d923672a5ecac994a3
Mykhailo <micha21cloz@gmail.com>
2024-02-23 21:30:34 +0000

    basic project setup
    Create basic fs setup Makefiles, integrate some frameworks and libs + start backend code

--- server/src/user/user_controllers.c
+++ server/src/user/user_controllers.c
@@ -1,24 +1,18 @@
 #include "user.h"
 
 void mx_user_ctrl_get(t_connection *c, t_http_message *m) {
-    //parse into params
-    //validate params
-    t_user *user = mx_user_get(1);
+  // parse into params
+  // validate params
+  t_user *user = mx_user_get(1);
 
-    //user struct to json
-    //send response
-    //send events to clients if necessary(for example "client new message created please update your data")
+  // user struct to json
+  // send response
+  // send events to clients if necessary(for example "client new message created
+  // please update your data")
 }
 
-void mx_user_ctrl_post(t_connection *c, t_http_message *m) {
+void mx_user_ctrl_post(t_connection *c, t_http_message *m) {}
 
-}
+void mx_user_ctrl_put(t_connection *c, t_http_message *m) {}
 
-void mx_user_ctrl_put(t_connection *c, t_http_message *m) {
-
-}
-
-void mx_user_ctrl_delete(t_connection *c, t_http_message *m) {
-
-}
-
+void mx_user_ctrl_delete(t_connection *c, t_http_message *m) {}
--- server/src/user/user_service.c
+++ server/src/user/user_service.c
@@ -1,18 +1,11 @@
 #include "user.h"
 
 t_user *mx_user_get(int id) {
-    //data from database attached to t_user in this function
+  // data from database attached to t_user in this function
 }
 
-t_user *mx_user_create(t_user_create_dto *dto) {
+t_user *mx_user_create(t_user_create_dto *dto) {}
 
-}
+t_user *mx_user_put(int id, t_user_put_dto *dto) {}
 
-t_user *mx_user_put(int id, t_user_put_dto *dto) {
-
-}
-
-t_user *mx_user_delete(int id) {
-
-}
-
+t_user *mx_user_delete(int id) {}

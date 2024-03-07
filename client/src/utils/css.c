66808af45626e980de01ce446765f4990f3a56fc
Maksym <72664331+maxkrv@users.noreply.github.com>
2024-03-07 20:25:36 +0200

    theme switcher (#9)
    * chore: theme switcher setup
    
    * Fix the display of theme switcher
    
    Add some margin-top and styles for theme_switcher
    
    ---------
    
    Co-authored-by: Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>

--- client/src/utils/css.c
+++ client/src/utils/css.c
@@ -9,3 +9,22 @@ void load_css(GtkWidget *window, const gchar *css_path) {
                                               GTK_STYLE_PROVIDER_PRIORITY_USER);
     gtk_widget_reset_style(window);
 }
+
+static void on_theme_switch(GtkComboBox *combo_box, GtkWidget *window) {
+    GtkTreeIter iter;
+    GtkTreeModel *model = gtk_combo_box_get_model(combo_box);
+    gchar *theme_name;
+
+    if (gtk_combo_box_get_active_iter(combo_box, &iter)) {
+        gtk_tree_model_get(model, &iter, 0, &theme_name, -1);
+        gchar *css_path = g_strconcat("client/static/styles/", theme_name, ".css", NULL);
+        load_css(window, css_path);
+        g_free(css_path);
+    }
+}
+
+void init_theme_switcher(GtkBuilder *builder, GtkWidget *window) {
+    GtkWidget *combo_box = GTK_WIDGET(gtk_builder_get_object(builder, "theme_switcher"));
+
+    g_signal_connect(combo_box, "changed", G_CALLBACK(on_theme_switch), window);
+}

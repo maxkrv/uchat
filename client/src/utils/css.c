0dc396b72886d0bd54c7b705c3b8d4073d4155c8
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-03-17 15:30:36 +0200

    Recreate theme switcher (#15)
    Replace GtkComboBoxText with GtkSwitcher

--- client/src/utils/css.c
+++ client/src/utils/css.c
@@ -10,21 +10,19 @@ void load_css(GtkWidget *window, const gchar *css_path) {
     gtk_widget_reset_style(window);
 }
 
-static void on_theme_switch(GtkComboBox *combo_box, GtkWidget *window) {
-    GtkTreeIter iter;
-    GtkTreeModel *model = gtk_combo_box_get_model(combo_box);
-    gchar *theme_name;
-
-    if (gtk_combo_box_get_active_iter(combo_box, &iter)) {
-        gtk_tree_model_get(model, &iter, 0, &theme_name, -1);
-        gchar *css_path = g_strconcat("client/static/styles/", theme_name, ".css", NULL);
-        load_css(window, css_path);
-        g_free(css_path);
+static void on_theme_switch(GtkSwitch *G_GNUC_UNUSED theme_switch, gboolean state, GtkWidget *window) {
+    gchar *css_path;
+    if (state) {
+        css_path = g_strdup("client/static/styles/dark.css");
+    } else {
+        css_path = g_strdup("client/static/styles/light.css");
     }
+    load_css(window, css_path);
+    g_free(css_path);
 }
 
 void init_theme_switcher(GtkBuilder *builder, GtkWidget *window) {
-    GtkWidget *combo_box = GTK_WIDGET(gtk_builder_get_object(builder, "theme_switcher"));
+    GtkWidget *switch_widget = GTK_WIDGET(gtk_builder_get_object(builder, "theme_switcher"));
 
-    g_signal_connect(combo_box, "changed", G_CALLBACK(on_theme_switch), window);
+    g_signal_connect(switch_widget, "state-set", G_CALLBACK(on_theme_switch), window);
 }

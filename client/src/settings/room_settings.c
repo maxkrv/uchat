d51bff0eb0f4035abb544767389ad8214d6947c6
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-04-02 18:01:08 +0300

    feat: room settings

---
+++ client/src/settings/room_settings.c
@@ -0,0 +1,37 @@
+#include "../../inc/client.h"
+
+static void on_cancel_room_settings(GtkButton *button, gpointer user_data) {
+    GtkWidget *dialog = GTK_WIDGET(user_data);
+    gtk_widget_hide(dialog);
+    (void)button;
+}
+
+void show_room_settings_dialog(GtkButton *button, gpointer user_data) {
+    GtkBuilder *builder = GTK_BUILDER(user_data);
+    GtkWidget *dialog = GTK_WIDGET(
+        gtk_builder_get_object(builder, "room_settings_dialog"));
+    GtkWidget *cancel_button = GTK_WIDGET(
+        gtk_builder_get_object(builder, "room_settings_cancel"));
+    // GtkWidget *submit_button = GTK_WIDGET(
+    //     gtk_builder_get_object(global_builder, "room_settings_submit"));
+    // GtkWidget *entry_username = GTK_WIDGET(
+    //     gtk_builder_get_object(global_builder, "room_settings_entry_room_name"));
+    // GtkWidget *entry_description = GTK_WIDGET(
+    //     gtk_builder_get_object(global_builder, "room_settings_entry_description"));
+
+    gtk_window_set_transient_for(
+        GTK_WINDOW(dialog),
+        GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(button))));
+    gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER_ON_PARENT);
+
+    g_signal_connect(cancel_button, "clicked",
+        G_CALLBACK(on_cancel_room_settings), dialog);
+
+    gint response = gtk_dialog_run(GTK_DIALOG(dialog));
+
+    if (response == GTK_RESPONSE_ACCEPT) {
+        g_print("OK\n");
+    } else if (response == GTK_RESPONSE_CANCEL) {
+        g_print("Cancel\n");
+    }
+}

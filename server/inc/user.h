d7b574a03743c70d25d5a1a995af85398ec076d6
Mykhailo <micha21cloz@gmail.com>
2024-04-02 18:44:55 +0100

    feat: user find by name

--- server/inc/user.h
+++ server/inc/user.h
@@ -51,6 +51,7 @@ void mx_user_ctrl_delete_me(t_connection *c, t_http_message *m);
 t_list *mx_user_get_rooms(int user_id);
 t_user *mx_user_get(int id);
 t_user *mx_user_get_by_tag(char *tag);
+t_user *mx_user_get_by_name(char *name);
 t_user *mx_user_create(t_user_create_dto *dto);
 t_user *mx_user_change_password(int id, char *password);
 t_user *mx_user_put(int id, t_user_update_dto *dto);

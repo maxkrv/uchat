0449633b0a8648f83a05248b83b50476b1fde182
Mykhailo <micha21cloz@gmail.com>
2024-04-07 10:59:50 +0100

    feat: direct room

--- client/src/room/create_room.c
+++ client/src/room/create_room.c
@@ -75,7 +75,7 @@ void on_submit_create_contact(GtkButton *button, gpointer user_data) {
     dto->name = g_strdup(room_name);
     dto->description = g_strdup("Private chat");
 
-    t_response *response = mx_sdk_room_post(dto);
+    t_response *response = mx_sdk_room_direct_post(dto, new_contact_name);
 
     if (mx_is_response_error(response)) {
         g_print("Error: %s\n", mx_sdk_exception_get_message(response));
@@ -84,32 +84,10 @@ void on_submit_create_contact(GtkButton *button, gpointer user_data) {
     }
 
     t_room *room = response->data;
-    t_response *user_response = mx_sdk_user_find_by_name(new_contact_name);
-
-    if (mx_is_response_error(user_response)) {
-        g_print("Error: %s\n", mx_sdk_exception_get_message(response));
-        mx_sdk_response_free(user_response, (t_func_free)mx_user_free);
-        return;
-    }
-
-    t_user *user = user_response->data;
-
-    t_response *add_member_response =
-        mx_sdk_room_member_post(room->id, user->id, true);
-
-    if (mx_is_response_error(add_member_response)) {
-        g_print("Error: %s\n", mx_sdk_exception_get_message(response));
-        mx_sdk_response_free(add_member_response,
-                             (t_func_free)mx_room_member_free);
-        return;
-    }
 
     mx_push_front(&global_rooms, room);
 
     mx_sdk_response_free(response, (t_func_free)mx_room_free);
-    mx_sdk_response_free(user_response, (t_func_free)mx_user_free);
-    mx_sdk_response_free(add_member_response,
-                         (t_func_free)mx_room_member_free);
     gtk_widget_hide(dialog);
     (void)button;
 }

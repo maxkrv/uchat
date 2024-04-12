186356386658f18a6a23430aec2ee45747e84c92
Yana <ylevchenko@stud.khpi.ucode-connect.study>
2024-04-12 20:32:26 +0300

    Scrolled room

--- client/src/chat/chat_stack.c
+++ client/src/chat/chat_stack.c
@@ -1,5 +1,19 @@
 #include "client.h"
 
+static void set_chat_scrollbar_to_bottom(GtkWidget *scrolled_chat_window) {
+    GtkAdjustment *adjustment = gtk_scrolled_window_get_vadjustment(
+        GTK_SCROLLED_WINDOW(scrolled_chat_window));
+    double upper = gtk_adjustment_get_upper(adjustment);
+    double page_size = gtk_adjustment_get_page_size(adjustment);
+    gtk_adjustment_set_value(adjustment, upper - page_size);
+}
+
+gboolean call_scrollbar_once(gpointer data) {
+    GtkWidget *scrolled_chat_window = GTK_WIDGET(data);
+    set_chat_scrollbar_to_bottom(scrolled_chat_window);
+    return G_SOURCE_REMOVE;
+}
+
 void show_selected_room(t_room *room) {
     GtkWidget *chat_container =
         GTK_WIDGET(gtk_builder_get_object(global_builder, "chat_container"));
@@ -24,4 +38,12 @@ void show_selected_room(t_room *room) {
 
     render_messages(room->id);
     init_message_form(room->id);
+
+    GtkWidget *scrolled_window = GTK_WIDGET(
+        gtk_builder_get_object(global_builder, "scrolled_chat_window"));
+
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
+                                   GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    g_timeout_add_full(G_PRIORITY_DEFAULT, 21,
+                       call_scrollbar_once, scrolled_window, NULL);
 }

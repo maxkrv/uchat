1e7ad2f9083d6208fff0ac7306851dcb68d3c536
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-04-19 13:56:18 +0300

    style: room image

--- client/src/room/room_image.c
+++ client/src/room/room_image.c
@@ -2,13 +2,15 @@
 
 void set_room_photo(t_room *room, GtkWidget *image) {
     t_response *response_user = mx_sdk_user_get_me();
-
+    
     if (mx_is_response_error(response_user)) {
         g_print("Error: %s\n", mx_sdk_exception_get_message(response_user));
         mx_sdk_response_free(response_user, (t_func_free)mx_user_free);
         return;
     }
+
     t_response *response_members = mx_sdk_room_members_get(room->id);
+
     if (mx_is_response_error(response_members)) {
         g_print("Error: %s\n", mx_sdk_exception_get_message(response_members));
         mx_list_free((t_list **)&response_members->data,
@@ -16,7 +18,9 @@ void set_room_photo(t_room *room, GtkWidget *image) {
         mx_sdk_response_free(response_members, NULL);
         return;
     }
+
     t_user *user = response_user->data;
+    
     if (g_strcmp0(room->type, "direct") == 0) {
         t_user *other_user = NULL;
 
@@ -26,38 +30,37 @@ void set_room_photo(t_room *room, GtkWidget *image) {
 
             if (member->user->id != user->id) {
                 other_user = member->user;
-
                 break;
             }
         }
 
-        if (other_user != NULL) {
-            GdkPixbuf *pixbuf = load_pixbuf_from_url(other_user->photo->url);
-            if (pixbuf != NULL) {
-                gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
-                g_object_unref(pixbuf);
-            } else {
-                g_printerr("Error loading image from URL\n");
-            }
-        } else {
+        if (other_user == NULL) {
             g_print("Error: Other user not found.\n");
+            return;
         }
 
+        GdkPixbuf *pixbuf = load_pixbuf_from_url(other_user->photo->url);
+        if (pixbuf == NULL) {
+            g_printerr("Error loading image from URL\n");
+            return;
+        }
+
+        gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+        g_object_unref(pixbuf);
     } else {
         GdkPixbuf *pixbuf = load_pixbuf_from_url(room->photo->url);
-        if (pixbuf != NULL) {
-            gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
-            g_object_unref(pixbuf);
-        } else {
+        if (pixbuf == NULL) {
             gtk_image_set_from_file(GTK_IMAGE(image),
                                 "client/static/images/avatar.png");
+            return;
         }
+
+        gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+        g_object_unref(pixbuf);
     }
 
     mx_sdk_response_free(response_user, (t_func_free)mx_user_free);
-
     mx_list_free((t_list **)&response_members->data,
                  (t_func_free)mx_room_member_free);
-
     mx_sdk_response_free(response_members, NULL);
 }
